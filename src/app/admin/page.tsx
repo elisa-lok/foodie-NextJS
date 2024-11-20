41f0117c862c5fcb25cdd82ba9d347c46ce2119f
Elisa <elisalok413@gmail.com>
2024-11-21 09:47:19 +1300

    check adminLogin in home webpage

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,15 +3,27 @@
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import './admin.css';
+import { checkAdminLogin } from "@/utils/auth";
 
 export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    const token = localStorage.getItem("admin_token"); 
-    if (!token) {
-      router.replace("/admin/login");
-    }
+    const verifyAdminLogin = async () => {
+      const token = localStorage.getItem("admin_token");
+      if (!token) {
+        router.replace("/admin/login");
+        return;
+      }
+
+      const response = await checkAdminLogin(token);
+      if (response.data.status !== 200) {
+        localStorage.removeItem("admin_token");
+        router.replace("/admin/login");
+      }
+    };
+
+    verifyAdminLogin();
   }, [router]);
 
   return (
