e6a32079848419d8030fac4e71314263403a7a9e
Elisa <elisalok413@gmail.com>
2024-12-04 09:47:06 +1300

    update product api

---
+++ src/app/api/admin/products/[productId]/route.js
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import dbConnect from "@/utils/db";
+import Meals from "@/app/models/Meals";
+import Admin from "@/app/models/Admin";
+import jwt from "jsonwebtoken";
+
+export async function GET(req) {
+  try {
+    const authHeader = req.headers.get("authorization");
+    if (!authHeader) {
+      return NextResponse.json({
+        status: 401,
+        error: "Authorization token is missing.",
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    await dbConnect();
+
+    const admin = await Admin.findOne({ _id: decoded.userId });
+    if (!admin) {
+      return NextResponse.json({ status: 404, error: "Admin not found" });
+    }
+
+    const products = await Meals.find();
+
+    if (!products || products.length === 0) {
+      return NextResponse.json({ status: 404, error: "No product found." });
+    }
+
+    return NextResponse.json({ status: 200, products: products });
+  } catch (error) {
+    return NextResponse.json({
+      status: 500,
+      error: error.message || "Internal Server Error",
+    });
+  }
+}
